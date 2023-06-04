2c052d1318eb14bd4b1f4f21bc5a9d2d94db14e2
Anclagen <91462616+Anclagen@users.noreply.github.com>
2023-06-05 01:24:17 +0200

    updated app with components

--- src/App.svelte
+++ src/App.svelte
@@ -1,25 +1,44 @@
 <script lang="ts">
-  import Router, { link } from "svelte-spa-router";
-  import { routes } from "./routes.js";
-  import { onMount } from "svelte";
-  import { Alert } from "flowbite-svelte";
-  let data;
-  async function fetchData() {
-    const response = await fetch(import.meta.env.VITE_API_ENDPOINT);
-    data = await response.json();
-  }
-  onMount(fetchData);
+	import Banner from './lib/components/main/Banner.svelte';
+	import About from './lib/components/main/About.svelte';
+	import Contact from './lib/components/main/Contact.svelte';
+	import Projects from './lib/components/main/Projects.svelte';
+	import Skills from './lib/components/main/Skills.svelte';
+	import Footer from './lib/components/layout/Footer.svelte';
+  import Nav from './lib/components/layout/Nav.svelte';
+	import { onMount } from 'svelte';
+
+	let navLinks: NodeListOf<HTMLAnchorElement>;
+	onMount(() => {
+		window.addEventListener('scroll', updateNavActiveLink);
+		const sections: NodeListOf<HTMLElement> = document.querySelectorAll(
+			'#home, #about, #skills, #projects, #contact'
+		);
+		navLinks = document.querySelectorAll('.nav-links');
+		updateNavActiveLink();
+
+		//https://dev.to/areeburrub/change-nav-link-s-style-as-you-scroll-4p62
+		function updateNavActiveLink() {
+			let currentPosition: string | null = '';
+			sections.forEach((section: HTMLElement) => {
+				const currentSection = section.offsetTop;
+				if (window.scrollY >= currentSection - 400) {
+					currentPosition = section.getAttribute('id');
+				}
+			});
+			navLinks.forEach((a) => {
+				a.classList.remove('underline');
+				if (currentPosition && a.hash.includes(currentPosition)) {
+					a.classList.add('underline');
+				}
+			});
+		}
+	});
 </script>
-
-<main class="p-4">
-  <Router {routes} />
-
-  {#if data}
-    <p>Data:</p>
-    <ul>
-      {#each Object.entries(data) as [key, value]}
-        <li>{key}: {value}</li>
-      {/each}
-    </ul>
-  {/if}
-</main>
+<Nav />
+<Banner />
+<About />
+<Skills />
+<Projects />
+<Contact />
+<Footer />
