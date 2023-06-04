8aa3dbb2ef31e4b343b4caa2068cb9d02a1615e4
Anclagen <91462616+Anclagen@users.noreply.github.com>
2023-06-05 01:21:17 +0200

    added data files

---
+++ src/lib/data/skills.ts
@@ -0,0 +1,23 @@
+export const skills = [
+	{ name: 'HTML5', imgSrc: 'https://skillicons.dev/icons?i=html' },
+	{ name: 'CSS3', imgSrc: 'https://skillicons.dev/icons?i=css' },
+	{ name: 'SASS', imgSrc: 'https://skillicons.dev/icons?i=sass' },
+	{ name: 'BootStrap', imgSrc: 'https://skillicons.dev/icons?i=bootstrap' },
+	{ name: 'Tailwind', imgSrc: 'https://skillicons.dev/icons?i=tailwind' },
+	{ name: 'Flowbite', imgSrc: '/assets/images/flowbite-logo.svg' },
+	{ name: 'Skeleton', imgSrc: '/assets/images/skeleton_ui.png' },
+	{ name: 'JavaScript', imgSrc: 'https://skillicons.dev/icons?i=javascript' },
+	{ name: 'TypeScript', imgSrc: 'https://skillicons.dev/icons?i=typescript' },
+	{ name: 'React', imgSrc: 'https://skillicons.dev/icons?i=react' },
+	{ name: 'Svelte', imgSrc: 'https://skillicons.dev/icons?i=svelte' },
+	{ name: 'Node', imgSrc: 'https://skillicons.dev/icons?i=nodejs' },
+	{ name: 'Express', imgSrc: 'https://skillicons.dev/icons?i=express' },
+	{ name: 'Prisma', imgSrc: 'https://skillicons.dev/icons?i=prisma' },
+	{ name: 'Git', imgSrc: 'https://skillicons.dev/icons?i=git' },
+	{ name: 'GitHub', imgSrc: 'https://skillicons.dev/icons?i=github' },
+	{ name: 'WordPress', imgSrc: 'https://skillicons.dev/icons?i=wordpress' },
+	{ name: 'VSC', imgSrc: 'https://skillicons.dev/icons?i=vscode' },
+	{ name: 'Figma', imgSrc: 'https://skillicons.dev/icons?i=figma' },
+	{ name: 'Adobe XD', imgSrc: 'https://skillicons.dev/icons?i=xd' },
+	{ name: 'Photoshop', imgSrc: 'https://skillicons.dev/icons?i=ps' }
+];
