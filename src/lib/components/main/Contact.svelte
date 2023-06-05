6760acba84d0cb316587ed2dc759b71abd969f63
Anclagen <91462616+Anclagen@users.noreply.github.com>
2023-06-05 16:04:56 +0200

    added foot and contact sections

--- src/lib/components/main/Contact.svelte
+++ src/lib/components/main/Contact.svelte
@@ -1,43 +1,35 @@
 <script>
-	import { Heading } from 'flowbite-svelte';
-	import DividerLight from '../dividers/DividerLight.svelte';
+  import { Heading } from "flowbite-svelte";
+  import DividerLight from "../dividers/DividerLight.svelte";
 </script>
 
 <DividerLight id="contact" />
 <section>
-	<div class="dark:bg-background-grey">
-		<Heading
-			tag="h2"
-			class="text-5xl font-extrabold text-gray-900 dark:text-white w-full text-center text-primary dark:text-light-primary mb-10"
-		>
-			Contact Me
-		</Heading>
-		<div class="content-cont-style dark:text-white">
-			<div class="page-wrapper-small">
-				<p>Feel free to contact me using the methods below. I look forward to hearing from you.</p>
-			</div>
-			<div class="contact-methods page-wrapper-small">
-				<a href="mailto:alexanderbarrett189@gmail.com" rel="noopener noreferrer" target="_blank">
-					<!-- <img src="/assets//images/tool-icons/gmail.png" alt="Gmail icon" /> -->
-					<p>Gmail</p></a
-				>
-				<a href="https://www.linkedin.com/in/alexander-barrett-64568a47/" target="_blank">
-					<!-- <img src="/images/tool-icons/linkedin(1).png" alt="Linkedin icon" /> -->
-					<p>Linkedin</p></a
-				>
-				<a
-					href="https://discordapp.com/users/178264761199362048"
-					rel="noopener noreferrer"
-					target="_blank"
-				>
-					<!-- <img src="/assets//images/tool-icons/discord(1).png" alt="discord icon" /> -->
-					<p>Discord</p></a
-				>
-			</div>
-			<div class="contact-form page-wrapper-small">
-				<!-- needs a new hook up 
-              <div class="button-center"><button class="display-form">Contact Form</button></div> -->
-			</div>
-		</div>
-	</div>
+  <div class="max-w-screen-lg mx-auto p-6 smd:p-10 bg-light-primary dark:bg-background-grey mb-10">
+    <Heading tag="h2" class="text-5xl font-extrabold text-gray-900 dark:text-white w-full text-center text-primary dark:text-light-primary mb-10">Contact Me</Heading>
+    <div class="dark:text-white md:flex mb-10">
+      <div class="hidden md:block p-3 md:basis-4/12 md:mx-3">
+        <img class="rounded-3xl w-fit shadow-md dark:shadow-highlights" src="/assets/images/leos_all_ears.jpg" alt="My Dog Leo" />
+      </div>
+      <div class="md: basis-8/12 p-3 mx-auto text-center">
+        <div class="mb-5">
+          <p>Want to get in touch were all ears!</p>
+        </div>
+        <div class="flex justify-evenly">
+          <a href="mailto:alexanderbarrett189@gmail.com" rel="noopener noreferrer" target="_blank">
+            <img src="/assets//images/tool_icons/gmail.png" alt="Gmail icon" aria-hidden="true" class="w-16 h-16" />
+            Gmail</a
+          >
+          <a href="https://www.linkedin.com/in/alexander-barrett-64568a47/" target="_blank" class="mx-5">
+            <img src="assets/images/tool_icons/linkedin(1).png" alt="Linkedin icon" aria-hidden="true" class="w-16 h-16" />
+            Linkedin</a
+          >
+          <a href="https://discordapp.com/users/178264761199362048" rel="noopener noreferrer" target="_blank">
+            <img src="/assets//images/tool_icons/discord.svg" alt="discord icon" aria-hidden="true" class="w-16 h-16" />
+            Discord</a
+          >
+        </div>
+      </div>
+    </div>
+  </div>
 </section>
